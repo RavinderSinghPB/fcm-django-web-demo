c83d681abd6cebaa2a0e465a37bb4b5b8902d141
Ravinder Singh <44737672+RavinderSinghPB@users.noreply.github.com>
2022-07-25 08:04:17 +0000

    fcm init

--- frontend/firebase-messaging-sw.js
+++ frontend/firebase-messaging-sw.js
@@ -8,7 +8,7 @@ importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
-  'messagingSenderId': '343960876538'
+  'messagingSenderId': '566147790621'
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
